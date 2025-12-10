fd9b14309486db981a76d176e394516f6cbe1099
AbdulnAzeem <abdulazeem135bhs@gmail.com>
2025-12-10 11:52:34 +0530

    02-components

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>
+            <h1>
+                Abdul Azeem
+            </h1>
+            <p>
+                Lorem
+                FALKL
+                lkljlk
+                alklfal
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
